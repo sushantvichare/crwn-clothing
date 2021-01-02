d21d3cc6fae6bc38eb4f69a9f0e03bc9468d851f
sushantvichare <sushantrules.07@gmail.com>
2021-01-02 17:27:48 +0530

    updated cart-icon with count using selectors and reselect lib

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,20 +4,27 @@ import { connect } from 'react-redux';
 
 import {toggleCart} from '../../redux/cart/cart-actions';
 
+import {selectCartItemsCount} from '../../redux/cart/cart-selectors';
+
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 
 import './cart-icon.styles.scss';
 
 
-const CartIcon=({toggleCart})=>(
+const CartIcon=({toggleCart,itemCount})=>(
   <div className='cart-icon' onClick={toggleCart}>
     <ShoppingIcon className='shopping-icon'/>
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
   )
 
 const mapDispatchToProps=dispatch=>({
   toggleCart:()=>dispatch(toggleCart())
+});
+
+const mapStateToProps=(state)=>({
+  itemCount:selectCartItemsCount(state)
 })
-export default connect(null,mapDispatchToProps)(CartIcon);
+
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
