41f3364bbea1a89d92eb8f1a1b7a770b88227908
sushantvichare <sushantrules.07@gmail.com>
2021-01-03 07:33:04 +0530

    add remove item ,and increase and decrease feature in checkout item with actions

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,5 +1,6 @@
 import {CartActionTypes} from './cart-types';
-import {addItemToCart} from './cart-utils';
+import {addItemToCart,removeItemfromCart} from './cart-utils';
+
 
 
 const INITIAL_STATE={
@@ -19,6 +20,17 @@ const cartReducer=(state=INITIAL_STATE,action)=>{
                 ...state,
                 cartItems:addItemToCart(state.cartItems,action.payload)
             };
+        case CartActionTypes.CLEAR_ITEM:
+            return{
+                ...state,
+                cartItems:state.cartItems.filter(cartItem=>cartItem.id!==action.payload.id)
+            }
+        case CartActionTypes.REMOVE_ITEM:
+            return{
+                ...state,
+                cartItems:removeItemfromCart(state.cartItems,action.payload)
+                }
+
         default:
             return state;
     }
